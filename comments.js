821d21d78106d1cf504fe22a639cc2b3a9d4bf9e
ram-ui-dev <103244278+ram-ui-dev@users.noreply.github.com>
2024-09-10 07:21:04 +0000

    Third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// create web server
+const express = require('express');
+const app = express();
+
+// create a route
+app.get('/comments', (req, res) => {
+    res.send('This is the comments page');
+});
+
+// start the server
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000/comments');
+});
