309d7d7b4fd2b40d92a85adf312d434d7eb8f798
ram-ui-dev <103244278+ram-ui-dev@users.noreply.github.com>
2024-09-10 07:36:26 +0000

    third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,14 @@
 // create web server
 const express = require('express');
 const app = express();
+const port = 3000;
 
 // create a route
-app.get('/comments', (req, res) => {
-    res.send('This is the comments page');
+app.get('/comments', function (req, res) {
+  res.send('Comments will be displayed here');
 });
 
 // start the server
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000/comments');
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
